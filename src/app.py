4a0a50bce6a6d8d3fd7b7ba6b983be7e956ea523
Carter Springall <carter.springall01@gmail.com>
2023-06-13 20:18:47 +0100

    added video downloading and stream selection

--- src/app.py
+++ src/app.py
@@ -1,11 +1,13 @@
-from youtube_video import YoutubeVideo
-from PyQt5 import QtCore
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QPushButton, QHBoxLayout, QWidget
 
-class LinkInputWindow(QMainWindow):
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QPushButton, QHBoxLayout, QWidget, QLabel, QVBoxLayout, QFileDialog, QComboBox
+from pytube import YouTube
+import traceback
+
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        
+        self.data_window = None
         #Give the window a title
         self.setWindowTitle("Youtube Video Downloader")
 
@@ -19,30 +21,52 @@ class LinkInputWindow(QMainWindow):
         link_submit = QPushButton("Submit")
         link_submit.clicked.connect(self.link_submitted)
 
+        #Add the widgets to the layout
         layout.addWidget(self.link_input)
         layout.addWidget(link_submit)
 
-        widget = QWidget()
-        widget.setLayout(layout)
-        self.setCentralWidget(widget)
+        page = QWidget()
+        page.setLayout(layout)
+        self.setCentralWidget(page)
+
 
     def link_submitted(self):
         print(self.link_input.text())
         try:
-            video = YoutubeVideo(self.link_input.text())
+            video = YouTube(self.link_input.text())
             print("SUCCESS! VALID LINK!")
+            self.data_window = VideoDataWindow(video)
+            self.data_window.show()
         except:
             print("ERROR! INVALID LINK!")
-        
 
-class VideoDataWindow(QMainWindow):
-    def __init__(self):
+
+class VideoDataWindow(QWidget):
+    def __init__(self, video):
+        self.video = video
         super().__init__()
+        layout = QVBoxLayout()
+        self.select_stream = QPushButton("Download")
+        self.select_stream.clicked.connect(self.download_stream)
+        self.dropdown = QComboBox()
+        for stream in self.video.streams.filter(file_extension='mp4'):
+            s = self.dropdown.addItem(str(stream))
+        layout.addWidget(self.dropdown)
+        layout.addWidget(self.select_stream)
+        self.setLayout(layout)
 
-        
+    def download_stream(self):
+        index = self.dropdown.currentIndex()
+        stream = self.video.streams.filter(file_extension='mp4')[index]
+        file = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        stream.download(output_path=file)
+        self.close()
+            
+
+
 app = QApplication([])
 
-window = LinkInputWindow()
+window = MainWindow()
 window.show()
 
 app.exec()
--- src/youtube_video.py
+++
@@ -1,5 +0,0 @@
-from pytube import YouTube
-
-class YoutubeVideo():
-    def __init__(self, link):
-            self.video = YouTube(link)
